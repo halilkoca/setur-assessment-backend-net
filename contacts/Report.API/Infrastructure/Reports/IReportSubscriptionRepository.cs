b7eba968793ce82a2de52218b9819e5be31cb1fd
Halil Koca <ihalilkoca@gmail.com>
2022-04-15 00:39:12 +0300

    added IReportSubscriptionRepository

---
+++ contacts/Report.API/Infrastructure/Reports/IReportSubscriptionRepository.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using Report.API.Model;
+using SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Contact.API.Infrastructure.Reports
+{
+    public interface IReportSubscriptionRepository
+    {
+        IMongoCollection<LocationReport> LocationReports { get; }
+
+        Task<IEnumerable<LocationReport>> Get(BaseRequest model);
+        Task<LocationReport> Get(Guid id);
+        Task<LocationReport> Create(LocationReport model);
+    }
+}
---
+++ contacts/Report.API/Infrastructure/Reports/ReportSubscriptionRepository.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using SharedLibrary;
+using Report.API.Model;
+using Report.API;
+
+namespace Contact.API.Infrastructure.Reports
+{
+    public class ReportSubscriptionRepository : IReportSubscriptionRepository, IDisposable
+    {
+        public ReportSubscriptionRepository()
+        {
+            var client = new MongoClient(Startup.Configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(Startup.Configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            LocationReports = database.GetCollection<LocationReport>(Startup.Configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+
+        public IMongoCollection<LocationReport> LocationReports { get; }
+
+        public async Task<IEnumerable<LocationReport>> Get(BaseRequest model)
+        {
+            var sortt = Builders<LocationReport>.Sort.Ascending(x => x.CreatedOn);
+
+            var data = await LocationReports.Find(c => true)
+                .Sort(sortt)
+                .Skip((model.PageNumber - 1) * model.PageSize)
+                .Limit(model.PageSize)
+                .ToListAsync();
+
+            return data;
+        }
+
+        public async Task<LocationReport> Get(Guid id)
+        {
+            return await LocationReports.Find(c => c.UUID == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<LocationReport> Create(LocationReport model)
+        {
+            await LocationReports.InsertOneAsync(model);
+            return model;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
