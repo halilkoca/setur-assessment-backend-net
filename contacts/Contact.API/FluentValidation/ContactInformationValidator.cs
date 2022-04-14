38e234ea03b31b269f0d02b407fe8da834194f41
Halil Koca <ihalilkoca@gmail.com>
2022-04-14 23:14:34 +0300

    added FluentValidation to contact + contact information models

---
+++ contacts/Contact.API/FluentValidation/ContactInformationValidator.cs
@@ -0,0 +1,17 @@
+﻿using Contact.API.Model;
+using FluentValidation;
+
+namespace Contact.API.FluentValidation
+{
+    public class ContactInformationValidator : AbstractValidator<ContactInformationModel>
+    {
+        public ContactInformationValidator()
+        {
+            RuleFor(p => p.Value)
+                .NotEmpty().WithMessage("{Value} is required.")
+                .NotNull()
+                .MinimumLength(3).WithMessage("{Value} must not exceed 3 characters.")
+                .MaximumLength(50).WithMessage("{Value} must not exceed 50 characters.");
+        }
+    }
+}
---
+++ contacts/Contact.API/FluentValidation/ContactValidation.cs
@@ -0,0 +1,30 @@
+﻿using Contact.API.Model;
+using FluentValidation;
+
+namespace Contact.API.FluentValidation
+{
+    public class ContactModelValidator : AbstractValidator<ContactModel>
+    {
+        public ContactModelValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{Name} is required.")
+                .NotNull()
+                .MinimumLength(3).WithMessage("{Name} must not exceed 3 characters.")
+                .MaximumLength(50).WithMessage("{Name} must not exceed 50 characters.");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("{LastName} is required.")
+                .NotNull()
+                .MinimumLength(3).WithMessage("{LastName} must not exceed 3 characters.")
+                .MaximumLength(50).WithMessage("{LastName} must not exceed 50 characters.");
+
+            RuleFor(p => p.Firm)
+                .NotEmpty().WithMessage("{Firm} is required.")
+                .NotNull()
+                .MinimumLength(3).WithMessage("{Firm} must not exceed 3 characters.")
+                .MaximumLength(50).WithMessage("{Firm} must not exceed 50 characters.");
+
+        }
+    }
+}
