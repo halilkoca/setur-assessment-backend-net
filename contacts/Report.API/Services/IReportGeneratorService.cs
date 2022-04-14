77ededb8b9f748a909ddab8d7cc2fe9124746699
Halil Koca <ihalilkoca@gmail.com>
2022-04-15 00:39:29 +0300

    added ReportGeneratorService

---
+++ contacts/Report.API/Services/IReportGeneratorService.cs
@@ -0,0 +1,15 @@
+﻿using Report.API.Model;
+using SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Report.API.Services
+{
+    public interface IReportGeneratorService
+    {
+        Task<List<LocationReport>> GetList(BaseRequest request);
+        Task<LocationReport> Get(System.Guid reportId);
+        Task<Guid> Create();
+    }
+}
---
+++ contacts/Report.API/Services/ReportGeneratorService.cs
@@ -0,0 +1,26 @@
+﻿using Report.API.Model;
+using SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Report.API.Services
+{
+    public class ReportGeneratorService : IReportGeneratorService
+    {
+        public Task<Guid> Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocationReport> Get(Guid reportId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LocationReport>> GetList(BaseRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
