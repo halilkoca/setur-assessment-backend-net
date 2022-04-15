729aa5905c91de877608cff315b52cc131981db6
Halil Koca <ihalilkoca@gmail.com>
2022-04-15 17:03:53 +0300

    added LocationReportConsumer

--- contacts/Contact.API/EventBusConsumer/LocationReportConsumer.cs
+++ contacts/Contact.API/EventBusConsumer/LocationReportConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Contact.API.Infrastructure.Reports;
 using EventBus.Messages.Events;
 using MassTransit;
 using System;
@@ -6,18 +7,20 @@ using System.Threading.Tasks;
 
 namespace Contact.API.EventBusConsumer
 {
-    public class LocationReportConsumer : IConsumer<LocationReportEventList>
+    public class LocationReportConsumer : IConsumer<LocationReportEvent>
     {
         private readonly IMapper _mapper;
+        private readonly ILocationReportRepository _locationReportRepository;
 
-        public LocationReportConsumer(IMapper mapper)
+        public LocationReportConsumer(IMapper mapper, ILocationReportRepository locationReportRepository)
         {
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _locationReportRepository = locationReportRepository ?? throw new ArgumentNullException(nameof(locationReportRepository));
         }
 
-        public async Task Consume(ConsumeContext<LocationReportEventList> context)
+        public async Task Consume(ConsumeContext<LocationReportEvent> context)
         {
-            // burada generate et ve db ye kaydet
+            await _locationReportRepository.GenerateAndSave(context.Message.LocationReportDetail.Id);
         }
     }
 }
