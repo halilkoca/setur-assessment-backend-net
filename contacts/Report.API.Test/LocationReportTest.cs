12dc78315241712b136715ada5102fb4b883516b
Halil Koca <ihalilkoca@gmail.com>
2022-04-17 12:53:03 +0300

    addded report api. _reportGeneratorService unit test

---
+++ contacts/Report.API.Test/LocationReportTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Report.API.Services;
+using SharedLibrary.Model;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Report.API.Test
+{
+    public class LocationReportTest
+    {
+        IServiceProvider services;
+        private readonly IReportGeneratorService _reportGeneratorService;
+
+        public LocationReportTest()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddMock<IReportGeneratorService>();
+            services = serviceCollection.BuildServiceProvider();
+            _reportGeneratorService = services.GetService<IReportGeneratorService>();
+        }
+
+        [Fact]
+        public async Task Generate_Successfully()
+        {
+            var response = Setup_Model();
+            services.GetMock<IReportGeneratorService>()
+                .Setup(x => x.Generate())
+                .Returns(Task.FromResult(response));
+
+            await _reportGeneratorService.Generate();
+
+        }
+
+        private LocationReportModel Setup_Model()
+        {
+            return new LocationReportModel
+            {
+                UUID = "625bde1b3e26d37f227081ff",
+                Status = ReportStatus.Preparing,
+                CreatedOn = DateTime.UtcNow
+            };
+        }
+    }
+}
